ca71e196659a6ed83d8781b27baa126e4393b7ad
Zhenyapas <zhekazhekapas@gmail.com>
2025-04-06 13:54:48 +0300

    Finished the project with a  description code comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -13,7 +14,11 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
@@ -44,6 +49,9 @@ module.exports = {
       patterns: [
         { from: 'data', to: 'data' }  
       ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/main.css'
     })
   ],
   devServer: {
